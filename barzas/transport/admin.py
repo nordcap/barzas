3b450babd44f28f3f60c9143550f82fdf3bc4a2c
Alexey Budaev <budaevaa@mail.ru>
2022-07-07 21:19:56 +0700

    Добавление моделей

--- barzas/transport/admin.py
+++ barzas/transport/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Point, Distance
+
+
+# пункты загрузки-выгрузки
+@admin.register(Point)
+class PointAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+# дистанция между пунктами загрузки-выгрузки
+@admin.register(Distance)
+class DistanceAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'point_start', 'point_final', 'distance')
+    list_editable = ('point_start', 'point_final', 'distance',)
+    list_filter = ('point_start', 'point_final',)
+    ordering = ('point_start', 'point_final',)
+    search_fields = ('point_start__name', 'point_final__name',)
+    search_help_text = "Поиск по пунктам погрузки/разгрузки"
--- barzas/transport/models.py
+++ barzas/transport/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+# точка погрузки или выгрузки самосвалов
+class Point(models.Model):
+    name = models.CharField(verbose_name='Наименование',
+                            db_index=True,
+                            unique=True,
+                            max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Пункт погрузки/выгрузки"  # имя в админке в ед.числе
+        verbose_name_plural = "Пункты погрузки/выгрузки"  # имя в админке в мн.числе
+        ordering = ['name']  # сортировка по возрастанию
+
+
+# Таблица расстояний между точками загрузки-разгрузки
+class Distance(models.Model):
+    point_start = models.ForeignKey(Point,
+                                    verbose_name='Пункт загрузки',
+                                    related_name='pointStart',
+                                    on_delete=models.PROTECT)
+    point_final = models.ForeignKey(Point,
+                                    verbose_name='Пункт разгрузки',
+                                    related_name='pointFinal',
+                                    on_delete=models.PROTECT)
+    distance = models.DecimalField(verbose_name='Расстояние',
+                                   max_digits=2,
+                                   decimal_places=1,
+                                   default=0.0)
+
+    def __str__(self):
+        return f"{self.point_start}->{self.point_final}"
+
+    class Meta:
+        verbose_name = "Расстояние между складами"
+        verbose_name_plural = "Расстояние между складами"
+        ordering = ['point_start', 'point_final']
