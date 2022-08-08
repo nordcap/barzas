99aa91ecbaa0dc56920c31eba40bb8d923d32fe9
Alexey Budaev <budaevaa@mail.ru>
2022-08-08 20:41:29 +0700

    Замена materialize на bootstrap

--- barzas/static/js/main.js
+++
@@ -1,9 +0,0 @@
-  M.AutoInit();
-
-  $(document).ready(function(){
-    console.log('jQuery loaded!');
-//    $('.tabs').tabs();
-    $('#test').click(function(){
-        alert('oajdsfoadj');
-    });
-  });
